78758488d8f0ff98a52e4531189d7488a006164e
Julien Ammous <j.ammous@gmail.com>
2011-05-05 18:21:08 +0200

    enabled disqus comments

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -7,15 +7,3 @@ $(function(){
 });
 
 
-// disqus comments count
-//
-// (function() {
-//   var links = document.getElementsByTagName('a');
-//   var query = '?';
-//   for(var i = 0; i < links.length; i++) {
-//     if(links[i].href.indexOf('#disqus_thread') >= 0) {
-//       query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-//     }
-//   }
-//   document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/#{disqus_shortname}/get_num_replies.js' + query + '"></' + 'script>');
-// })();
